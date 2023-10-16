e1f3ab35563111ab116652b89c53f5e2a70747ac
Mustafizur Rahman <mustafizur232@gmail.com>
2023-10-16 17:22:25 +0600

    spinner loaded

--- js/phone.js
+++ js/phone.js
@@ -3,10 +3,8 @@ const loadPhones = async (searchBtnField) => {
     const data = await res.json();
     const phones = data.data
     showPhones(phones);
-    // console.log(phones);
 }
 const showPhones = (phones) => {
-    // console.log(phones)
     const showMoreBtn = document.getElementById('show-more-button');
     if (phones.length > 12) {
         showMoreBtn.classList.remove('hidden')
@@ -18,34 +16,42 @@ const showPhones = (phones) => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
     phones.forEach(phone => {
-        // console.log(phone);
         const phoneDiv = document.createElement('div');
         phoneDiv.classList = 'card p-4 bg-gray-100 shadow-xl';
         phoneDiv.innerHTML = `
-        
           <figure><img src="${phone.image}" alt="Shoes" /></figure>
           <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
            <div class="card-actions justify-end">
           <button class="btn btn-primary">Buy Now</button>
-            </div>
-             </div>`;
+          </div>
+           </div>`;
         phoneContainer.appendChild(phoneDiv);
-    })
+    });
+    toggleLoadingSpinner(false);
 
 }
 const searchHandle = () => {
-    // console.log('search')
+    toggleLoadingSpinner(true)
     const searchBtn = document.getElementById('search-btn');
     const searchBtnField = searchBtn.value;
-    // console.log(searchBtnField);
     loadPhones(searchBtnField);
     searchBtn.value = '';
 }
 const searchHandle2 = () => {
+    toggleLoadingSpinner(true)
     const searchBtn = document.getElementById('search-btn2');
     const searchBtnField = searchBtn.value;
     loadPhones(searchBtnField);
     searchBtn.value = '';
+}
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if (isLoading) {
+        loadingSpinner.classList.remove('hidden')
+    }
+    else {
+        loadingSpinner.classList.add('hidden')
+    }
 }
