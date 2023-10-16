d9a552eed2027c780e00fa81f0280cebb75e2731
Mustafizur Rahman <mustafizur232@gmail.com>
2023-10-16 15:46:57 +0600

    phone displayed

---
+++ js/phone.js
@@ -0,0 +1,37 @@
+const loadPhones = async (searchBtnField) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchBtnField}`)
+    const data = await res.json();
+    const phones = data.data
+    showPhones(phones);
+    // console.log(phones);
+}
+const showPhones = (phones) => {
+    // console.log(phones)
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
+    phones.forEach(phone => {
+        // console.log(phone);
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList = 'card p-4 bg-gray-100 shadow-xl';
+        phoneDiv.innerHTML = `
+        
+          <figure><img src="${phone.image}" alt="Shoes" /></figure>
+          <div class="card-body">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+           <div class="card-actions justify-end">
+          <button class="btn btn-primary">Buy Now</button>
+            </div>
+             </div>`;
+        phoneContainer.appendChild(phoneDiv);
+    })
+
+}
+const searchHandle = () => {
+    // console.log('search')
+    const searchBtn = document.getElementById('search-btn');
+    const searchBtnField = searchBtn.value;
+    // console.log(searchBtnField);
+    loadPhones(searchBtnField);
+    searchBtn.value = '';
+}
